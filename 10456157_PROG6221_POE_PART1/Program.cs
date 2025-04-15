561618033a1b53dd1b17ceb384ce1efec3d701e2
Keshav Haripaul <keshav.haripaul@yahoo.com>
2025-04-15 14:23:13 +0200

    added main files and project structure

--- 10456157_PROG6221_POE_PART1/Program.cs
+++ 10456157_PROG6221_POE_PART1/Program.cs
@@ -6,7 +6,7 @@ namespace CyberAwarenessChatBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("CyberAwarenessChatBot initialized.");
+            Console.WriteLine("CyberAwarenessChatBot initialized");
         }
     }
 }
